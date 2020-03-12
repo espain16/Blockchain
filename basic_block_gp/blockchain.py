a1e7b0602e4f71386735b50c81ff098bd544bcc9
Elise Spain <elisespain@gmail.com>
2020-03-12 13:50:53 -0700

    Chain endpoint setup

--- basic_block_gp/blockchain.py
+++ basic_block_gp/blockchain.py
@@ -39,9 +39,11 @@ class Blockchain(object):
         }
 
         # Reset the current list of transactions
+        self.current_transactions = []
         # Append the chain to the block
+        self.chain.append(block)
         # Return the new block
-        pass
+        return block
 
     def hash(self, block):
         """
@@ -52,9 +54,11 @@ class Blockchain(object):
         """
 
         # Use json.dumps to convert json into a string
+        string_block = json.dumps(block, sort_keys=True)
         # Use hashlib.sha256 to create a hash
         # It requires a `bytes-like` object, which is what
         # .encode() does.
+        raw_hash = hashlib.sha256(string_block.encode())
         # It converts the Python string into a byte string.
         # We must make sure that the Dictionary is Ordered,
         # or we'll have inconsistent hashes
@@ -68,9 +72,10 @@ class Blockchain(object):
         # This can be hard to read, but .hexdigest() converts the
         # hash to a string of hexadecimal characters, which is
         # easier to work with and understand
+        hex_hash =raw_hash.hexdigest()
 
         # TODO: Return the hashed block string in hexadecimal format
-        pass
+        return hex_hash        
 
     @property
     def last_block(self):
@@ -132,7 +137,8 @@ def mine():
 def full_chain():
     response = {
         # TODO: Return the chain and its current length
-        'message': 'hello'
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
     }
     return jsonify(response), 200
 
